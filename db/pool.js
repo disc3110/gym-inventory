31765215cdd296ddb3dafc4180766da484f1578c
Diego Ivan Solis Cuevas <disc3110@gmail.com>
2026-01-14 12:28:39 -0800

    change pool for production

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,9 @@
 const { Pool } = require("pg");
 
-const user = process.env.DB_USER;  // This should be read from an environment variable
-const password = process.env.DB_PASSWORD;  // This should be read from an environment variable
-const host = process.env.DB_HOST;  // This should be read from an environment variable
-const database = process.env.DB_NAME;
-const port = process.env.DB_PORT || 5432;
-
-// Again, this should be read from an environment variable
 const pool = new Pool({
-  connectionString: `postgresql://${user}:${password}@${host}:${port}/${database}`
+  connectionString: process.env.DATABASE_URL,
+  // optional for Railway:
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 pool.on("error", (err) => {
